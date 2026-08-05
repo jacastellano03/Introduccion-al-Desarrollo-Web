026f5b403ee6a7768edd45204f54f26f4dbfbe24
jacastellano03 <jacastellano@puce.edu.ec>
2026-08-04 20:56:01 -0500

    Refactor menu structure and modal functions

--- restauwithjava/restau/script.js
+++ restauwithjava/restau/script.js
@@ -46,8 +46,78 @@ function cerrarModal() {
 
 function mostrarOpciones() {
 
-    const tipo=document.getElementById('tipoMenu').value;
-    const div=document.getElementById('opcionesMenu');
-    div.innerHTML='';
-    
+    const tipo = document.getElementById('tipoMenu').value;
+    const div = document.getElementById('opcionesMenu');
+
+    div.innerHTML = '';
+
+    if (!tipo) {
+
+        return;
+
+    }
+
+    const opciones = menu[tipo];
+
+    let html = `<h3>Plato principal</h3>`;
+
+    opciones.platos.forEach((plato, posicion) => {
+
+        html += `
+            <label>
+                <input
+                    type="radio"
+                    name="plato"
+                    value="${plato}"
+                    ${posicion === 0 ? 'checked' : ''}
+                >
+                ${plato}
+            </label>
+            <br>
+        `;
+
+    });
+
+
+    html += `<h3>Acompañamientos</h3>`;
+
+    opciones.acompanamientos.forEach((acompanamiento) => {
+
+        html += `
+            <label>
+                <input
+                    type="checkbox"
+                    name="acompanamiento"
+                    value="${acompanamiento}"
+                >
+                ${acompanamiento}
+            </label>
+            <br>
+        `;
+
+    });
+
+
+    html += `<h3>Bebidas</h3>`;
+
+    opciones.bebidas.forEach((bebida, posicion) => {
+
+        html += `
+            <label>
+                <input
+                    type="radio"
+                    name="bebida"
+                    value="${bebida}"
+                    ${posicion === 0 ? 'checked' : ''}
+                >
+                ${bebida}
+            </label>
+            <br>
+        `;
+
+    });
+
+
+    div.innerHTML = html;
+
 }
